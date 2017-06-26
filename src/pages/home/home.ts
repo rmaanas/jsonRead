c377609b704ffa4f8e60dbb965ad50ff3d3128db
rmaanas <manas.krishna85@gmail.com>
2017-06-26 18:33:46 +0530

    JsonRead
    send and recieve json data from server

---
+++ src/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import { NavController } from 'ionic-angular';
+import {LoadingController} from 'ionic-angular';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  data: any;
+  loader: any;
+  jsonObj: any;
+  authenticated: any;
+  constructor(public loadingCtrl : LoadingController,public navCtrl: NavController, public http: Http) {
+        this.data = {};
+        this.data.username = '';
+        this.data.response = '';
+        this.http = http;
+  }
+ 
+  submit() {
+	var link = 'http://Sample-env-1.i23yadcngp.us-west-2.elasticbeanstalk.com/testrest/login';
+	var data = JSON.stringify({username: this.data.username, password: this.data.password});
+	var headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  this.presentLoading();
+	this.http.post(link, data, {headers: headers})
+	.subscribe(data => {
+		this.data.response = data["_body"];
+    this.jsonObj = JSON.parse(data["_body"]);
+    this.authenticated = this.jsonObj.authenticated;
+    this.loader.dismiss();
+	}, error => {
+		console.log("Oooops!");
+	});
+  }
+
+  presentLoading() {
+      this.loader = this.loadingCtrl.create({
+      content: "Authenticating...",
+    });
+    this.loader.present();
+  }
+}
