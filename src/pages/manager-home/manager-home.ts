f6e86a711c6a710a3ee332357acb6cf291364f86
pranavbhoyar <pranavbhoyar96@gmail.com>
2017-06-27 13:23:42 +0530

    Add files via upload

---
+++ src/pages/manager-home/manager-home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { App, MenuController } from 'ionic-angular';
+
+/**
+ * Generated class for the ManagerHomePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-manager-home',
+  templateUrl: 'manager-home.html',
+})
+export class ManagerHomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,app: App, menu: MenuController) {
+  	menu.enable(true);
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ManagerHomePage');
+  }
+
+}
