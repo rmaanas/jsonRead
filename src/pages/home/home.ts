38a7d67752a1643001ad678ea46f1f0bc28768cc
pranavbhoyar <pranavbhoyar96@gmail.com>
2017-06-28 11:42:25 +0530

    view history page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import {Http, Headers} from '@angular/http';
 import { NavController } from 'ionic-angular';
 import {LoadingController} from 'ionic-angular';
 import 'rxjs/add/operator/map';
+import { ManagerHomePage } from '../manager-home/manager-home';
+//import {MyApp } from '../../app/app.component';
 
 @Component({
   selector: 'page-home',
@@ -13,6 +15,7 @@ export class HomePage {
   data: any;
   loader: any;
   jsonObj: any;
+ // myApp: MyApp;
   authenticated: any;
   constructor(public loadingCtrl : LoadingController,public navCtrl: NavController, public http: Http) {
         this.data = {};
@@ -22,20 +25,41 @@ export class HomePage {
   }
  
   submit() {
-	var link = 'http://Sample-env-1.i23yadcngp.us-west-2.elasticbeanstalk.com/testrest/login';
-	var data = JSON.stringify({username: this.data.username, password: this.data.password});
-	var headers = new Headers();
+<<<<<<< HEAD
+  var link = 'http://Sample-env-1.i23yadcngp.us-west-2.elasticbeanstalk.com/testrest/login';
+  var data = JSON.stringify({username: this.data.username, password: this.data.password});
+  var headers = new Headers();
   headers.append("Content-Type", "application/json");
   this.presentLoading();
-	this.http.post(link, data, {headers: headers})
-	.subscribe(data => {
-		this.data.response = data["_body"];
+  this.http.post(link, data, {headers: headers})
+  .subscribe(data => {
+    this.data.response = data["_body"];
     this.jsonObj = JSON.parse(data["_body"]);
     this.authenticated = this.jsonObj.authenticated;
     this.loader.dismiss();
-	}, error => {
-		console.log("Oooops!");
-	});
+    if(this.authenticated=="yes")
+    {
+      this.navCtrl.setRoot(ManagerHomePage);
+    }
+  }, error => {
+    console.log("Oooops!");
+  });
+=======
+  	var link = 'http://Sample-env-1.i23yadcngp.us-west-2.elasticbeanstalk.com/testrest/login';
+  	var data = JSON.stringify({username: this.data.username, password: this.data.password});
+  	var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    this.presentLoading();
+  	this.http.post(link, data, {headers: headers})
+  	.subscribe(data => {
+  		this.data.response = data["_body"];
+      this.jsonObj = JSON.parse(data["_body"]);
+      this.authenticated = this.jsonObj.authenticated;
+      this.loader.dismiss();
+  	}, error => {
+  		console.log("Oooops!");
+  	});
+>>>>>>> c86a565490d6d35e06fc377794ddb21fdb6f3a5f
   }
 
   presentLoading() {
