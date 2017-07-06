c4a71e6c87f172fb47e0822b94016cf5abb90319
pranavbhoyar <pranavbhoyar96@gmail.com>
2017-07-06 12:20:32 +0530

    n

--- src/pages/projectpage/projectpage.ts
+++ src/pages/projectpage/projectpage.ts
@@ -13,6 +13,8 @@ collectings=null;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.collectings=this.getCollectings();
+
+   
   }
 
   ionViewDidLoad() {
@@ -22,33 +24,33 @@ collectings=null;
   return [
     {
       "Event"     : "Pickup the visitor",
-      "Start"     : "8:00",
-      "End"       : "9:00",
-      "Date"      : "29/06/2017",
+      "Start"     : "08:00",
+      "End"       : "09:00",
+      "Date"      : "2017-06-26",
       "Owner"     : "Anil",
       "Venue"     : "Airport"
     },
     {
       "Event"    : "Travel to Hotel",
-      "Start"     : "9:00",
-      "End"       : "9:15",
-      "Date"      : "29/06/2017",
+      "Start"     : "09:00",
+      "End"       : "09:15",
+      "Date"      : "2017-06-26",
       "Owner"     : "Sunil",
       "Venue"     : "Cab"
     },
     {
       "Event"    : "Meet at Hotel",
-      "Start"     : "9:15",
-      "End"       : "9:30",
-      "Date"      : "29/06/2017",
+      "Start"     : "09:15",
+      "End"       : "09:30",
+      "Date"      : "2017-06-26",
       "Owner"     : "Ram",
       "Venue"     : "Hotel Taj"
     },
     {
       "Event"     : "Travel to  Atos",
-      "Start "    :  "9:30",
-      "End"       :  "9:45",
-      "Date"      :  "29/06/2017",
+      "Start"    :  "09:31",
+      "End"       :  "09:45",
+      "Date"      :  "2017-06-26",
       "Owner"     :  "Sunil",
       "Venue"     :  "Cab"
     },
@@ -56,7 +58,7 @@ collectings=null;
       "Event"     : "Meeting",
       "Start"     :  "10:00",
       "End"       : "12:00",
-      "Date"      : "29/06/2017",
+      "Date"      : "2017-06-26",
       "Owner"     : "Rajesh",
       "Venue"     : "Atos, Pune"
     }
@@ -64,21 +66,52 @@ collectings=null;
   }
 
 
-  itemSelected(item,item1) {
+  itemSelected(item) {
+  /*
+    var dob: Date = item.Date;
+    console.log(dob);
+    var newDate=new Date(dob);
+    var day=newDate.getDate();
+    var month=newDate.getMonth();
+    month=month+1;
+    var year = newDate.getFullYear();
+    if(month<10)
+    {
+      var mo = "0"+month;
+      month=parseInt(mo);
+      var da = year + "-" + mo + "-" + day;
+    }
+    else
+    {
+      var da = year + "-" + month + "-" + day;
+    }
+    console.log(mo);
+    item.Date=da;
+*/
     this.navCtrl.push(AddEditPage,{
-    item : 'Edit',
-    item1 : item1
+    item : 'Edit Event',
+    item1 : item
     });
     console.log(item.Event + " is selected");
   }
 
-  fab(item)
+  fab()
   {
+
+    var item:any ;
+    item = {
+      "Event"     : "",
+      "Start"     :  "00:00",
+      "End"       : "00:00",
+      "Date"      : "2017-07-28",
+      "Owner"     : "",
+      "Venue"     : ""
+    }
     this.navCtrl.push(AddEditPage,{
-    item:'Add',
-    item1:null
+    item:'Add An Event',
+    item1:item
     });
-    console.log(item+"has reached");
+    console.log("Add has reached");
   }
   
 
