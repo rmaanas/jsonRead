d7bbda7ea2f83781c9e4f3e90405e585e2c178ac
Reginold Antony Kolenchery <reginold.antony@yahoo.com>
2017-07-12 12:03:07 +0530

    c

--- src/pages/history1/history1.ts
+++ src/pages/history1/history1.ts
@@ -13,8 +13,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'history1.html',
 })
 export class History1Page {
+loader:any;
+  username: any;
+  myjsonObj: any;
+  jsonObj: any;
+  submitAttempt: boolean = false;
+  visit : any;
+  allprojects :any;
+  addVisitForm : FormGroup;
+  status : any;
+  currdate : Date;
+  projectname:any = '';
+  clienthead:any = '';
+  organisation:any = '';
+  clientemail:any = '';
+  currVisit: any;
+  venue : any ;
+  visitdate : any ;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+ constructor(public nav: Nav,public navCtrl: NavController,public http : Http,public navParams: NavParams, public formBuilder: FormBuilder, public loadingCtrl: LoadingController, public storage: Storage, public alertCtrl: AlertController) {
+    
+    this.presentLoading();
+    this.getData();
+  }
+
+  getData()
+  {
+      return this.storage.get("currVisit").then(value=>{
+        this.currVisit = value;
+        this.projectname = this.currVisit.NAME;
+        this.clienthead =  this.currVisit.CLIENTHEAD;
+        this.organisation= this.currVisit.ORGANISATION;
+        this.clientemail = this.currVisit.CLIENTEMAIL;
+        this.visitdate  =  this.currVisit.Venue;
+        this.venue      =  this.currVisit.VISITDATE;
+        //this.addVisitForm.controls['venue'].setValue = this.currVisit.VENUE;
+        this.loader.dismiss();
+      });    
+  }
+
+  presentLoading() 
+  {
+      this.loader = this.loadingCtrl.create({
+      content: "Loading Visit Details...",
+      });
+      this.loader.present();
   }
 
   ionViewDidLoad() {
