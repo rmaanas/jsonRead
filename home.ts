76b5095a97828c6e9fb2c04e90d8d3ae896db643
khaireprajakta563 <khaireprajakta563@gmail.com>
2017-07-13 10:00:51 +0530

    splash screen added

---
+++ home.ts
@@ -0,0 +1,130 @@
+﻿import { Component } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import { NavController } from 'ionic-angular';
+import {LoadingController} from 'ionic-angular';
+import 'rxjs/add/operator/map';
+import {ManagerHomePage} from '../manager-home/manager-home';
+import {EmployeeHomePage} from '../employee-home/employee-home';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+   splash = true;
+  public imglink = "https://upload.wikimedia.org/wikipedia/en/thumb/0/01/Atos.svg/800px-Atos.svg.png";
+  
+  loader: any;
+  jsonObj: any;
+  authenticated: any;
+  loginForm : FormGroup;
+  
+  /*
+  myDate: any = new Date().toISOString().split('T')[0];
+  myDate:any = new Date();
+  newDate:any = this.myDate.getFullYear() + '-' + (this.myDate.getMonth()+1) + '-' + this.myDate.getDate();
+  
+  date:any = new Date();
+  day:any = ('0' + this.date.getDate()).slice(-2);
+  month:any = ('0' + (this.date.getMonth() + 1)).slice(-2);
+  year:any = this.date.getFullYear();
+  currDate: any = this.year + '-' + this.month + '-' + this.day;
+  currTime:any = this.date.getTime();
+  hours:any;
+  minutes:any;
+  */
+
+  public error_mssg : any = null;
+
+  constructor(public storage : Storage,public loadingCtrl : LoadingController,public navCtrl: NavController, public formBuilder : FormBuilder, public http : Http) {
+    
+        this.loginForm = formBuilder.group({
+        'username': [null,Validators.compose([Validators.required,Validators.maxLength(50)])],
+        'password': [null,Validators.compose([Validators.required, Validators.maxLength(50)])]
+    });
+  }
+
+     ionViewDidLoad() {
+    setTimeout(() => this.splash = false, 4000);
+  }
+
+ 
+  submit(value : any) {
+  	var link = 'http://localhost:9000/TestRest/testrest/login';
+  	var data = JSON.stringify({username: value.username, password: value.password});
+  	
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    
+    this.presentLoading();
+  	
+    this.http.post(link, data, {headers: headers})
+  	.subscribe(data => {
+    	value.response = data["_body"];
+      this.jsonObj = JSON.parse(data["_body"]);
+      this.authenticated = this.jsonObj.authenticated;
+      this.loader.dismiss();
+
+      if(this.authenticated == "yes")
+      {
+        this.storage.set("jsonObj", this.jsonObj);
+        if(this.jsonObj.role == "manager")
+        {
+          console.log(this.jsonObj.username);
+          this.navCtrl.setRoot(ManagerHomePage).then(
+          ()=>{
+          this.navCtrl.popToRoot();
+          }
+          );
+        }
+        else
+        {
+          this.navCtrl.setRoot(EmployeeHomePage).then(
+          ()=>{
+          this.navCtrl.popToRoot();
+          }
+          );
+        }
+      }
+      else
+	  {
+         this.error_mssg = "Invalid Credentials! Please try again!";
+      }
+    },error => {
+    		console.log("Error!");
+    });
+  }
+
+  presentLoading() {
+      this.loader = this.loadingCtrl.create({
+      content: "Authenticating...",
+    });
+    this.loader.present();
+  }
+
+/*  
+  printCurrDate()
+  {
+    this.getCurrDate();
+    console.log("current date is " + this.currDate + " and time is " + this.currTime);
+    console.log("current time is among noon or afternoon? " + (this.currTime > "12:00"));
+  }
+  
+  getCurrDate()
+  {
+        this.date = new Date();
+        this.day = ('0' + this.date.getDate()).slice(-2);
+        this.month = ('0' + (this.date.getMonth() + 1)).slice(-2);
+        this.year = this.date.getFullYear();
+        this.currDate = this.year + '-' + this.month + '-' + this.day;
+        this.hours = ('0' + this.date.getHours()).slice(-2);
+        this.minutes = ('0' + this.date.getMinutes()).slice(-2);
+        this.currTime = this.hours + ":" + this.minutes;
+        //console.log(" getCurr: current date is " + this.currDate + " and time is " + this.currTime);    
+  }
+*/
+
+}
